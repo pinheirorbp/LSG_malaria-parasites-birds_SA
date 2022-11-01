4b99487991544e6c4227eba79aa4993c4fb5abe9
Pinheiro <hs07xude@idiv.de>
2022-11-01 14:28:47 +0100

    updating correlation tests (vocabulary)

--- script3_correlations.R
+++ script3_correlations.R
@@ -16,9 +16,6 @@ for (i in c(1:2,4:8)){
   ablineclip(lm(com_data$invconn[BIO==i]~com_data$Latitude[BIO==i]), col=COLORS[i],x1=min(com_data$Latitude[BIO==i]),x2=max(com_data$Latitude[BIO==i]))
 }
 cor.test(com_data$invconn,com_data$Latitude)
-#Partner specialization (residuals)
-m1=glm(invconn~spar*shost, family="quasibinomial", data=com_data)
-cor.test(residuals(m1),com_data$Latitude)
 #Frequency specialization
 plot(com_data$Latitude,com_data$H2, pch=16,col=COLORS[BIO])
 abline(lm(com_data$H2~com_data$Latitude))
@@ -26,9 +23,6 @@ for (i in c(1:2,4:8)){
   ablineclip(lm(com_data$H2[BIO==i]~com_data$Latitude[BIO==i]), col=COLORS[i],x1=min(com_data$Latitude[BIO==i]),x2=max(com_data$Latitude[BIO==i]))
 }
 cor.test(com_data$H2,com_data$Latitude)
-#Frequency specialization (residuals)
-m1=glm(H2~spar*shost, family="quasibinomial", data=com_data)
-cor.test(residuals(m1),com_data$Latitude)
 #Phylogenetic specialization
 plot(com_data$Latitude,com_data$DSI, pch=16,col=COLORS[BIO])
 abline(lm(com_data$DSI~com_data$Latitude))
@@ -36,7 +30,3 @@ for (i in c(1:2,4:8)){
   ablineclip(lm(com_data$DSI[BIO==i]~com_data$Latitude[BIO==i]), col=COLORS[i],x1=min(com_data$Latitude[BIO==i]),x2=max(com_data$Latitude[BIO==i]))
 }
 cor.test(com_data$DSI,com_data$Latitude)
-#Phylogenetic specialization(residuals)
-m1=glm(DSI~spar*shost, family="quasibinomial", data=com_data)
-cor.test(residuals(m1),com_data$Latitude)
-rm(BIO,COLORS,i,m1)
